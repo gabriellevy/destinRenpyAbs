0fd0c40e304eb0c5b80430d1ab22a0f9ea4e5458
gabriellevy <mdeharbe@hotmail.com>
2021-11-28 19:30:23 +0100

    structure de base via destin Clovis

---
+++ abs/declencheur.py
@@ -0,0 +1,43 @@
+from abs import proba
+from abs import modifProba
+from abs import condition
+
+class Declencheur:
+    """
+    Classe gérant le déclenchement d'événement particulier via leurs conditions et probas,
+    calculs en fonction des caracs de la situation actuelle
+    """
+
+    def __init__(self, aproba, labelGoTo):
+        """
+        aproba peut être une proba complète déclarée ou juste un float dans ce cas il sera convertie en proba déclarée dans ce constructeur
+        """
+        self.conditions_ = []
+        if isinstance(aproba, proba.Proba):
+            self.proba_ = aproba
+        else:
+            # la proba est un simple nombre flottant (sans modif de proba)
+            self.proba_ = proba.Proba(aproba)
+        self.labelGoTo_ = labelGoTo
+
+    def calculerProba(self, situation):
+        for condition in self.conditions_:
+            resTest = condition.tester(situation)
+            if not resTest:
+                return 0. # si une des conditions n'est pas vérifiée alors la proba est égale à 0
+            pass
+
+        return self.proba_.calculer(situation)
+
+    def executer(self):
+        return self.labelGoTo_
+
+    def AjouterCondition(self, condition):
+        self.conditions_.append(condition)
+
+    def AjouterConditions(self, conditions):
+        """
+        tableau de conditions ajoutées une à une
+        """
+        for condObj in conditions:
+            self.conditions_.append(condObj)
---
+++ abs/humanite/portrait.py
@@ -0,0 +1,36 @@
+import random
+
+class Portrait:
+
+    C_PORTRAIT = u"Portrait"
+
+    def DeterminerPortraitPersoPrincipal(self, situation):
+        """
+        retourne l'adresse du portrait à afficher pour le perso courant
+        """
+        ageAnnees = situation.AgeEnAnnees()
+
+        return self.DeterminerPortraits(situation, ageAnnees)
+
+    def DeterminerPortraits(self, situation, ageAnnees):
+        """
+        retourne l'adresse du portrait à afficher pour le perso courant
+        """
+        portraits = []
+        portraitCourant = situation.GetValCarac(Portrait.C_PORTRAIT)
+
+        # if ageAnnees >= 30 and ageAnnees <= 40:
+        #     portraits.append("images/portraits/clovis30_40.png")
+        # elif ageAnnees > 40:
+        #     portraits.append("images/portraits/clovis40+.png")
+        # else:
+        #     portraits.append("images/portraits/clovis15_30.png")
+        # portraits.append("images/portraits/celte20-40.png")
+
+        if len(portraits) == 0:
+            portraits = ["images/portraits/inconnu.jpg"]
+
+        if portraits.count(portraitCourant) == 0:
+            portraitCourant = random.choice(portraits)
+
+        return portraitCourant
